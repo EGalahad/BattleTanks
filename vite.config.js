2243f60ad676ee23c6f7997620d61b2bd008f5ef
elijah <2818195805@qq.com>
2023-12-19 15:56:03 +0800

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/BattleTanks-Fork/',
+  base: '/BattleTanks/',
   build: {
     assetsDir: 'assets',
   }
