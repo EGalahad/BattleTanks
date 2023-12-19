adf62a21c0a8b692532e06916d8297717f68f34c
elijah <2818195805@qq.com>
2023-12-19 15:51:18 +0800

    Delete dist asset files, update vite.config.js and package.json, and refactor Ground class constructor

--- src/World/World.ts
+++ src/World/World.ts
@@ -36,6 +36,7 @@ class World {
 
     mesh: { [key: string]: THREE.Group } = {};
     audio: { [key: string]: AudioBuffer } = {};
+    textures: { [key: string]: THREE.Texture } = {};
     listener: THREE.AudioListener;
     bgAudio: THREE.Audio;
 
@@ -62,7 +63,7 @@ class World {
         await new Promise(resolve => setTimeout(resolve, 1000));
         this.scene = new Scene();
 
-        this.ground = new Ground("main");
+        this.ground = new Ground("main", this.textures);
         this.scene.add(this.ground);
 
         this.hemiLight = new HemiSphereLight("main");
@@ -185,6 +186,26 @@ class World {
         });
         audioLoader.load('assets/audio/explosion.mp3', (buffer) => {
             this.audio["Explosion"] = buffer;
+        });
+
+                const textureLoader = new THREE.TextureLoader();
+        textureLoader.load('assets/grassy-meadow1-bl/grassy-meadow1_albedo.png', (texture) => {
+            this.textures["albedo"] = texture;
+        });
+        textureLoader.load('assets/grassy-meadow1-bl/grassy-meadow1_ao.png', (texture) => {
+            this.textures["ao"] = texture;
+        });
+        textureLoader.load('assets/grassy-meadow1-bl/grassy-meadow1_height.png', (texture) => {
+            this.textures["height"] = texture;
+        });
+        textureLoader.load('assets/grassy-meadow1-bl/grassy-meadow1_metallic.png', (texture) => {
+            this.textures["metallic"] = texture;
+        });
+        textureLoader.load('assets/grassy-meadow1-bl/grassy-meadow1_normal-ogl.png', (texture) => {
+            this.textures["normal"] = texture;
+        });
+        textureLoader.load('assets/grassy-meadow1-bl/grassy-meadow1_roughness.png', (texture) => {
+            this.textures["roughness"] = texture;
         });
 
     }
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/BattleTanks-Fork/',
+  build: {
+    assetsDir: 'assets',
+  }
+})
